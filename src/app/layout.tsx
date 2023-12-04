bef6dd98f378a90a44576c6eecd1a8bf1f9b5e42
ev0clu <ev0clu@gmail.com>
2023-12-04 14:56:09 +0100

    Title is updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ import Navbar from '@/components/Navbar';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Food Ordering App',
+  title: 'Fooder',
   description: 'Next app for ordering food'
 };
 
