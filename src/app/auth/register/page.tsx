29ed1bf2e1ad2b3511ea72cd39ae1c583abbb80e
ev0clu <ev0clu@gmail.com>
2023-12-13 15:13:47 +0100

    Register page is created

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,213 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import ErrorMessage from '@/components/ErrorMessage';
+import { toast } from 'sonner';
+import { Button, buttonVariants } from '@/components/ui/button';
+import { Loader2 } from 'lucide-react';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { cn } from '@/lib/utils';
+
+const formSchema = z
+  .object({
+    username: z
+      .string()
+      .min(1, 'Username is required')
+      .max(10)
+      .trim(),
+    email: z
+      .string()
+      .min(1, 'Email is required')
+      .email('Invalid email')
+      .trim(),
+    street: z.string().min(1, 'Street is required').max(10).trim(),
+    city: z.string().min(1, 'City is required').max(10).trim(),
+    phone: z.string().min(1, 'Phone is required').max(10),
+    password: z
+      .string()
+      .min(1, 'Password is required')
+      .min(3, 'Password must have min 3 characters'),
+    confirmPassword: z
+      .string()
+      .min(1, 'Password confirmation is required')
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ['confirmPassword'],
+    message: 'Password do not match'
+  });
+
+type formType = z.infer<typeof formSchema>;
+
+const Register = () => {
+  const router = useRouter();
+  const [error, setError] = useState('');
+  const [isSubmitting, setSubmitting] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<formType>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+      email: '',
+      street: '',
+      city: '',
+      phone: '',
+      password: '',
+      confirmPassword: ''
+    }
+  });
+
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    try {
+      setError('');
+      setSubmitting(true);
+      const response = await fetch('/api/auth/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          username: data.username,
+          email: data.email,
+          street: data.street,
+          city: data.city,
+          phone: data.phone,
+          password: data.password
+        })
+      });
+      if (response.ok) {
+        router.push('/auth/login');
+      } else {
+        setSubmitting(false);
+        const body = await response.json();
+        if (body.message) {
+          setError(body.message);
+        } else {
+          setError('An unexpected error occurred');
+        }
+      }
+    } catch (error) {
+      setError('An unexpected error is occured');
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <>
+      {error && toast.error(error)}
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="mx-auto mb-8 mt-5 flex max-w-sm flex-col gap-3"
+      >
+        <div className="flex flex-col">
+          <Label htmlFor="username">Username</Label>
+          <Input
+            id="username"
+            autoComplete="username"
+            type="text"
+            placeholder="Username"
+            {...register('username')}
+          />
+          <ErrorMessage>{errors.username?.message}</ErrorMessage>
+        </div>
+        <div className="flex flex-col space-y-2">
+          <Label htmlFor="email">Email</Label>
+          <Input
+            id="email"
+            autoComplete="email"
+            type="email"
+            placeholder="email@example.com"
+            {...register('email')}
+          />
+          <ErrorMessage>{errors.email?.message}</ErrorMessage>
+        </div>
+        <div className="flex flex-col space-y-2">
+          <Label htmlFor="street">Street</Label>
+          <Input
+            id="street"
+            autoComplete="street"
+            type="text"
+            placeholder="street"
+            {...register('street')}
+          />
+          <ErrorMessage>{errors.street?.message}</ErrorMessage>
+        </div>
+        <div className="flex flex-col space-y-2">
+          <Label htmlFor="city">City</Label>
+          <Input
+            id="city"
+            autoComplete="city"
+            type="city"
+            placeholder="city"
+            {...register('city')}
+          />
+          <ErrorMessage>{errors.city?.message}</ErrorMessage>
+        </div>
+        <div className="flex flex-col space-y-2">
+          <Label htmlFor="phone">Phone</Label>
+          <Input
+            id="phone"
+            autoComplete="phone"
+            type="tel"
+            placeholder="+4859657"
+            {...register('phone')}
+          />
+          <ErrorMessage>{errors.phone?.message}</ErrorMessage>
+        </div>
+        <div className="flex flex-col space-y-2">
+          <Label htmlFor="password">Password</Label>
+          <Input
+            id="password"
+            autoComplete="off"
+            type="password"
+            placeholder="Password"
+            {...register('password')}
+          />
+          <ErrorMessage>{errors.password?.message}</ErrorMessage>
+        </div>
+        <div className="flex flex-col space-y-2">
+          <Label htmlFor="confirmPassword">Confirm Password</Label>
+          <Input
+            id="confirmPassword"
+            autoComplete="off"
+            type="password"
+            placeholder="Confirm Password"
+            {...register('confirmPassword')}
+          />
+          <ErrorMessage>
+            {errors.confirmPassword?.message}
+          </ErrorMessage>
+          <ErrorMessage>{errors.root?.message}</ErrorMessage>
+        </div>
+        <p>
+          Already have an account?{' '}
+          <Link
+            href="/auth/login"
+            className={cn(buttonVariants({ variant: 'link' }), 'p-1')}
+          >
+            Log in
+          </Link>
+        </p>
+        <Button
+          className="mx-auto w-28"
+          type="submit"
+          disabled={isSubmitting}
+        >
+          {isSubmitting && (
+            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+          )}
+          Register
+        </Button>
+      </form>
+    </>
+  );
+};
+
+export default Register;
