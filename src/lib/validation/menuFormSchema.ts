73095e290a68116e3b2ad697e90cabdd4cfb6b20
ev0clu <ev0clu@gmail.com>
2023-12-22 15:03:27 +0100

    menuFormSchema is created

---
+++ src/lib/validation/menuFormSchema.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod';
+
+export const menuFormSchema = z.object({
+  menuName: z.string().min(1, 'Menu name is required').max(20).trim(),
+  menuDescription: z
+    .string()
+    .min(1, 'Menu description is required')
+    .max(20)
+    .trim(),
+  menuSize: z.enum(['SMALL', 'NORMAL']),
+  menuCategory: z
+    .array(z.string())
+    .refine((value) => value.some((item) => item), {
+      message: 'You have to select at least one item.'
+    }),
+  menuPrice: z
+    .string()
+    .refine((val) => Number(val) > 0.1 && Number(val) < 999.9, {
+      message: 'Menu price should be between 0.1 ... 999.9'
+    })
+});
