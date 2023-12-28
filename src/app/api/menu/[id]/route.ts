fc3068296e5b9b3dfd23ad7463500ee27d8797bd
ev0clu <ev0clu@gmail.com>
2023-12-28 20:10:32 +0100

    GET menu based on ID api is created

---
+++ src/app/api/menu/[id]/route.ts
@@ -0,0 +1,61 @@
+import { NextResponse } from 'next/server';
+import prisma from '../../../../../prisma/client';
+
+export const revalidate = 0;
+
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    // Validation with safeParse
+    /* const validation = userSchema.safeParse(body);
+    if (!validation.success) {
+      NextResponse.json(validation.error.format(), { status: 400 });
+    }*/
+
+    const menuItem = await prisma.menu.findUnique({
+      where: { id: params.id },
+      include: { images: true, categories: true }
+    });
+
+    if (!menuItem) {
+      return NextResponse.json(
+        {
+          message: `Menu: ${params.id} not found`
+        },
+        { status: 404 }
+      );
+    }
+
+    // Remove the last 2 characters from the size property
+    const unformattedPrice = menuItem.price
+      .slice(0, -2)
+      .replace(/,/g, '.');
+
+    const modifiedMenuItem = {
+      ...menuItem,
+      price: unformattedPrice,
+      images: menuItem.images.map((image) => ({ ...image })),
+      categories: menuItem.categories.map((category) => ({
+        ...category,
+        menuIDs: [...category.menuIDs]
+      }))
+    };
+
+    return NextResponse.json(
+      {
+        menuItem: modifiedMenuItem,
+        message: `Menu: ${params.id} with records are returned`
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: error
+      },
+      { status: 500 }
+    );
+  }
+}
