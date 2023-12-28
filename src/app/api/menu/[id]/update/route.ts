d01ed29f724ef1dad26a6830e659db6c8a70f59c
ev0clu <ev0clu@gmail.com>
2023-12-28 20:11:18 +0100

    Menu update client side and API is created

---
+++ src/app/api/menu/[id]/update/route.ts
@@ -0,0 +1,112 @@
+import { NextResponse } from 'next/server';
+import prisma from '../../../../../../prisma/client';
+import { menuFormSchema } from '@/lib/validation/menuFormSchema';
+import { formatPrice } from '@/lib/utils';
+
+export const revalidate = 0;
+
+export async function PUT(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const body: typeof menuFormSchema = await req.json();
+
+    const {
+      menuName,
+      menuDescription,
+      menuImage, // Assuming menuImage is an array of objects with id and url properties
+      menuSize,
+      menuCategory,
+      menuPrice
+    } = menuFormSchema.parse(body);
+
+    const categoryIds: { id: string }[] = menuCategory.map(
+      (categoryId) => ({
+        id: categoryId
+      })
+    );
+
+    const formattedPrice = formatPrice(menuPrice, {
+      currency: 'EUR',
+      notation: 'compact'
+    });
+
+    // Retrieve existing images associated with the menu
+    const existingImages = await prisma.image.findMany({
+      where: { menuId: params.id }
+    });
+
+    // Extract URLs from existing images
+    const existingImageUrls = existingImages.map(
+      (image) => image.url
+    );
+
+    // Extract URLs from new menu images
+    const newMenuImageUrls = menuImage.map((image) => image.url);
+
+    // Find URLs to be removed (exist in existing but not in new)
+    const urlsToRemove = existingImageUrls.filter(
+      (url) => !newMenuImageUrls.includes(url)
+    );
+
+    // Find URLs to be added (exist in new but not in existing)
+    const urlsToAdd = newMenuImageUrls.filter(
+      (url) => !existingImageUrls.includes(url)
+    );
+
+    // Remove images with URLs that are not in the new array
+    await prisma.image.deleteMany({
+      where: {
+        menuId: params.id,
+        url: {
+          in: urlsToRemove
+        }
+      }
+    });
+
+    // Create new images with URLs that are not in the existing array
+    const newImages = urlsToAdd.map((url) => {
+      return prisma.image.create({
+        data: {
+          url,
+          menuId: params.id
+        }
+      });
+    });
+
+    // Perform the database operations
+    await prisma.$transaction([...newImages]);
+
+    const updatedMenu = await prisma.menu.update({
+      where: { id: params.id },
+      data: {
+        name: menuName,
+        description: menuDescription,
+        size: menuSize,
+        price: formattedPrice,
+        categories: {
+          set: categoryIds
+        }
+      },
+      include: {
+        categories: true,
+        images: true
+      }
+    });
+
+    return NextResponse.json(
+      {
+        message: `Menu id:${params.id} is updated`
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: error
+      },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/menu/[id]/update/page.tsx
@@ -0,0 +1,457 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { Category } from '@prisma/client';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form';
+import { toast } from 'sonner';
+import { z } from 'zod';
+import { useFieldArray, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Loader2, MinusCircle, PlusCircle } from 'lucide-react';
+import { menuFormSchema } from '@/lib/validation/menuFormSchema';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select';
+import { ExtendedMenu } from '@/types/menu';
+import { useRouter } from 'next/navigation';
+import { useSession } from 'next-auth/react';
+import Loading from '@/components/Loading';
+
+type MenuProps = {
+  menuItem: ExtendedMenu;
+};
+
+type ListCategoryProps = {
+  categoryList: Category[];
+};
+
+type formType = z.infer<typeof menuFormSchema>;
+
+const MenuEdit = ({ params }: { params: { id: string } }) => {
+  const router = useRouter();
+  const { status, data: session } = useSession();
+
+  const [isSubmitting, setSubmitting] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [menuItem, setMenuItem] = useState<ExtendedMenu>();
+  const [categoryList, setCategoryList] = useState<Category[]>([]);
+
+  const form = useForm<formType>({
+    resolver: zodResolver(menuFormSchema),
+    defaultValues: {
+      menuName: '',
+      menuDescription: '',
+      menuImage: [],
+      menuSize: undefined,
+      menuCategory: [],
+      menuPrice: ''
+    }
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    control: form.control,
+    name: 'menuImage'
+  });
+
+  const fetchCategory = async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(`/api/category`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        cache: 'no-cache'
+      });
+
+      if (response.ok) {
+        const data: ListCategoryProps = await response.json();
+        setCategoryList(data.categoryList);
+      } else {
+        setIsError(true);
+        setIsLoading(false);
+        toast.error('An unexpected error occurred');
+      }
+    } catch (error) {
+      setIsError(true);
+      setIsLoading(false);
+      toast.error('An unexpected error is occured');
+    }
+  };
+
+  const fetchMenu = async () => {
+    try {
+      const response = await fetch(`/api/menu/${params.id}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        cache: 'no-cache'
+      });
+
+      if (response.ok) {
+        const data: MenuProps = await response.json();
+        setMenuItem(data.menuItem);
+        setIsLoading(false);
+      } else {
+        setIsError(true);
+        setIsLoading(false);
+        toast.error('An unexpected error occurred');
+      }
+    } catch (error) {
+      setIsError(true);
+      setIsLoading(false);
+      toast.error('An unexpected error is occured');
+    }
+  };
+
+  useEffect(() => {
+    fetchCategory();
+    if (!isError) {
+      fetchMenu();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading) {
+      form.setValue('menuName', menuItem!.name);
+      form.setValue('menuDescription', menuItem!.description);
+      /*  menuItem!.images.forEach((image) => {
+        append({ url: image.url });
+      });*/
+      form.setValue('menuImage', menuItem!.images);
+      form.setValue('menuSize', menuItem!.size);
+      form.setValue('menuCategory', menuItem!.categoryIDs);
+      form.setValue('menuPrice', menuItem!.price);
+    }
+  }, [isLoading]);
+
+  const handleImgAddClick = () => {
+    if (fields.length < 3) {
+      append({ url: '' });
+    }
+  };
+
+  const handleImgRemoveClick = (index: number) => {
+    if (fields.length > 0) {
+      remove(index);
+    }
+  };
+
+  useEffect(() => {
+    if (form.formState.isSubmitSuccessful) {
+      form.reset({
+        menuName: '',
+        menuDescription: '',
+        menuImage: [],
+        menuSize: undefined,
+        menuCategory: [],
+        menuPrice: ''
+      });
+      /*  for (let i = 0; i < fields.length; i++) {
+        remove();
+      }*/
+
+      /* setMenuItem({
+        id:'',createdAt:'',updatedAt:'',
+        name: '',
+        description: '',
+        categoryIDs: [],
+        price: '',
+        images: [],
+        size: 'SMALL'
+      });*/
+    }
+  }, [form.formState.isSubmitSuccessful]);
+
+  const onSubmit = async (data: z.infer<typeof menuFormSchema>) => {
+    try {
+      setSubmitting(true);
+
+      const response = await fetch(`/api/menu/${params.id}/update`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          menuName: data.menuName,
+          menuDescription: data.menuDescription,
+          menuImage: data.menuImage,
+          menuSize: data.menuSize,
+          menuCategory: data.menuCategory,
+          menuPrice: data.menuPrice
+        })
+      });
+
+      if (response.ok) {
+        setSubmitting(false);
+        router.push(`/user/${session?.user.id}/admin`);
+        toast.success(
+          `Menu: ${data.menuName} has been updated successfully`
+        );
+      } else {
+        setSubmitting(false);
+        setSubmitting(false);
+        const body = await response.json();
+        if (body.message) {
+          toast.error(body.message);
+        } else {
+          toast.error('An unexpected error occurred');
+        }
+      }
+    } catch (error) {
+      setSubmitting(false);
+      setSubmitting(false);
+      toast.error('An unexpected error is occured');
+    }
+  };
+
+  if (status === 'unauthenticated') {
+    return router.push('/login');
+  }
+
+  if (status === 'loading') {
+    return <Loading />;
+  }
+
+  if (session?.user.role !== 'ADMIN') {
+    return (
+      <p className="text-base opacity-50">
+        You do not have <b>ADMIN</b> permission do view this page
+      </p>
+    );
+  }
+
+  return (
+    <div>
+      <h1 className="text-top my-5 text-4xl font-bold">Update</h1>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="mx-auto mb-8 mt-5 flex max-w-md flex-col gap-3"
+        >
+          <FormField
+            control={form.control}
+            name="menuName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input
+                    disabled={isSubmitting}
+                    placeholder="Menu name"
+                    type="text"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="menuDescription"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Input
+                    disabled={isSubmitting}
+                    placeholder="Menu description"
+                    type="text"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="menuImage"
+            render={() => (
+              <FormItem>
+                <div className="mb-4 flex flex-row justify-between">
+                  <div>
+                    <FormLabel className="mb-4 text-base">
+                      Image URL
+                    </FormLabel>
+                    <FormDescription>
+                      Maximum 3 URLs can be add
+                    </FormDescription>
+                  </div>
+                  <Button
+                    type="button"
+                    size="icon"
+                    onClick={handleImgAddClick}
+                  >
+                    <PlusCircle />
+                  </Button>
+                </div>
+                {fields.map((image, index) => (
+                  <FormField
+                    key={image.id}
+                    control={form.control}
+                    name={`menuImage.${index}.url`}
+                    render={({ field }) => {
+                      return (
+                        <FormItem
+                          key={image.id}
+                          className="flex flex-col items-stretch space-x-3 space-y-0"
+                        >
+                          <div className="flex flex-grow flex-row gap-4">
+                            <FormControl>
+                              <Input
+                                disabled={isSubmitting}
+                                placeholder="https://example.com/image1.jpg"
+                                type="text"
+                                {...field}
+                              />
+                            </FormControl>
+                            <Button
+                              type="button"
+                              size="icon"
+                              onClick={() =>
+                                handleImgRemoveClick(index)
+                              }
+                            >
+                              <MinusCircle />
+                            </Button>
+                          </div>
+                          <FormMessage />
+                        </FormItem>
+                      );
+                    }}
+                  />
+                ))}
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="menuSize"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Size</FormLabel>
+                <Select onValueChange={field.onChange}>
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select a size to display" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem value="SMALL">Small</SelectItem>
+                      <SelectItem value="NORMAL">Normal</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="menuCategory"
+            render={() => (
+              <FormItem>
+                <FormLabel className="mb-4 text-base">
+                  Category
+                </FormLabel>
+                <div className="grid grid-cols-3 gap-3">
+                  {categoryList.map((category) => (
+                    <FormField
+                      key={category.id}
+                      control={form.control}
+                      name="menuCategory"
+                      render={({ field }) => {
+                        return (
+                          <FormItem
+                            key={category.id}
+                            className="flex flex-row items-start space-x-3 space-y-0"
+                          >
+                            <FormControl>
+                              <Checkbox
+                                checked={field.value?.includes(
+                                  category.id
+                                )}
+                                onCheckedChange={(checked) => {
+                                  return checked
+                                    ? field.onChange([
+                                        ...field.value,
+                                        category.id
+                                      ])
+                                    : field.onChange(
+                                        field.value?.filter(
+                                          (value) =>
+                                            value !== category.id
+                                        )
+                                      );
+                                }}
+                              />
+                            </FormControl>
+                            <FormLabel className="font-normal">
+                              {category.name}
+                            </FormLabel>
+                          </FormItem>
+                        );
+                      }}
+                    />
+                  ))}
+                </div>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="menuPrice"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Price</FormLabel>
+                <FormControl>
+                  <Input
+                    disabled={isSubmitting}
+                    placeholder="Menu name"
+                    type="number"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex justify-end">
+            <Button
+              type="submit"
+              className="flex w-28 gap-1 text-right"
+              disabled={isSubmitting}
+            >
+              {isSubmitting && (
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              )}
+              Update
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default MenuEdit;
