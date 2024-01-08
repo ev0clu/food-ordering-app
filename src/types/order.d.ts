9c16d6be83b7e5b87d98fc57266c620d727610f4
ev0clu <ev0clu@gmail.com>
2024-01-08 16:13:24 +0100

    Order Type is updated

--- src/types/order.d.ts
+++ src/types/order.d.ts
@@ -3,6 +3,7 @@ import { CartItem } from '@/lib/store';
 export type OrderSchema = {
   cart: CartItem[];
   userId: string;
+  customerName: string;
   email: string;
   street: string;
   city: string;
