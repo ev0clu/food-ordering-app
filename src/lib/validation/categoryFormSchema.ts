0bf1c748c489081009f6d5201f5a8a6306a5cd67
ev0clu <ev0clu@gmail.com>
2024-01-11 16:46:31 +0100

    Category schema validation is updated

--- src/lib/validation/categoryFormSchema.ts
+++ src/lib/validation/categoryFormSchema.ts
@@ -1,9 +1,5 @@
 import { z } from 'zod';
 
 export const categoryFormSchema = z.object({
-  categoryName: z
-    .string()
-    .min(1, 'Category is required')
-    .max(20)
-    .trim()
+  categoryName: z.string().min(1, 'Category is required').trim()
 });
