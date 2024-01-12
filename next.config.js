6da51046aefc580d157b6c18bfb7f9d16850750f
ev0clu <ev0clu@gmail.com>
2024-01-12 11:29:21 +0100

    Updated images domain handling due to changes in Next.js version 14

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['www.allrecipes.com', 'imagesvc.meredithcorp.io']
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.allrecipes.com',
+        pathname: '**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'imagesvc.meredithcorp.io',
+        pathname: '**'
+      }
+    ]
   }
 };
 
